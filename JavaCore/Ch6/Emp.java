e7b8eb21cf1c81a6cf60ce53cbc8ed9b67355487
Narendra Nath Paul <npaul7345@gmail.com>
2023-07-31 11:55:05 +0530

    Add Emp.java under JavaCore/Ch6

---
+++ JavaCore/Ch6/Emp.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Emp {
+
+    int eid;
+    String ename;
+    String companyname;
+
+    static int id;
+    static String name;
+
+    static String company;
+
+    static void Run () {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the ID of the employee : ");
+        id = sc.nextInt();
+
+        System.out.print("Enter the name of the employee : ");
+        name = sc.next();
+
+        System.out.print("Enter the name of the employee's company : ");
+        company = sc.next();
+    }
+
+    public void input() {
+
+        eid = Emp.id;
+        ename = Emp.name;
+        companyname = Emp.company;
+    }
+
+    public void display() {
+
+        System.out.println();
+        System.out.println("The id of the employee is : " + eid);
+        System.out.println("The name of the employee is : " + ename);
+        System.out.println("THe name of the employee's company : " + companyname);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Emp e = new Emp();
+        Emp.Run();
+        e.input();
+        e.display();
+    }
+}

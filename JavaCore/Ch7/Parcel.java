6995274d67a86534fc8e0b6e9c707de66487b880
Narendra Nath Paul <npaul7345@gmail.com>
2023-08-02 10:43:41 +0530

    Modify Parcel.java under JavaCore/Ch7/

--- JavaCore/Ch7/Parcel.java
+++ JavaCore/Ch7/Parcel.java
@@ -10,16 +10,16 @@ public class Parcel {
         System.out.print("Enter the weight of the parcel (in grams) : ");
         int weight = sc.nextInt();
 
-        int charge = 15;
+        int price = 15;
 
         if (weight > 1000) {
 
             int count = ((weight - 1000) % 500 == 0) ? (weight - 1000) / 500 : (weight - 1000) / 500 + 1;
-            charge += count * 8;
+            price += count * 8;
         }
 
         System.out.println();
-        System.out.println("The charge of the parcel weighing " + weight + " is : " + charge);
+        System.out.println("The price of the parcel weighing " + weight + " is : " + price);
         System.out.println();
     }
 }

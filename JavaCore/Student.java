bfa8cfc9ee03a6034b804b778bf4d249f5158ff8
Narendra Nath Paul <npaul7345@gmail.com>
2023-08-17 20:13:37 +0530

    Modify Student.java cause thursday lab required a Student class hence old Student.java renamed to StudentNew.java

--- JavaCore/Student.java
+++ JavaCore/Student.java
@@ -1,23 +1,67 @@
-public class Student {
+import java.util.*;
 
-    String name;
-    int roll_no;
+class Person {
+    private String name;
+    private String address;
+    Person() {
 
-    Student(String name, int roll_no) {
-
-        this.name = name;
-        this.roll_no = roll_no;
+        name = null;
+        address = null;
     }
 
-    void display() {
+    public void setName(String name) {
+        this.name = name;
+    }
 
-        System.out.println("The name of the student is : " + name);
-        System.out.println("The roll no of the student is : " + roll_no);
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
+public class Student extends Person{
+
+    private int student_id;
+
+    Student (String name, String address, int stu_id) {
+
+        setName(name);
+        setAddress(address);
+        this.student_id = stu_id;
+    }
+
+    // Only to remove 'set data member to final' warning
+    public void setStudent_id(int student_id) {
+        this.student_id = student_id;
+    }
+
+    public void display() {
+
+        System.out.println();
+        System.out.println("The name of the student is : " + getName());
+        System.out.println("The address of the student is : " + getAddress());
+        System.out.println("The id of the student is : " + student_id);
     }
 
     public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
 
-        Student ob = new Student("John", 2);
-        ob.display();
+        System.out.println();
+        System.out.print("Enter the name of the Student : ");
+        String name = sc.nextLine();
+        System.out.print("Enter the address of the Student : ");
+        String address = sc.nextLine();
+        System.out.print("Enter the id of the Student : ");
+        int id = sc.nextInt();
+
+        Student s = new Student(name, address, id);
+        s.display();
     }
 }
